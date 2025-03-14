1896001f35b8cb38251dca621a707ab832a756a8
NoName9989 <130733726+NoName9989@users.noreply.github.com>
2025-03-14 17:36:40 +0700

    Update hg.lua

--- hg.lua
+++ hg.lua
@@ -38,8 +38,6 @@ local RunService = game:GetService("RunService")
 local LocalPlayer = Players.LocalPlayer
 local Camera = workspace.CurrentCamera
 
-local espEnabled = false -- Trạng thái bật/tắt ESP
-
 -- Nhảy Cao Toggle
 UtilityTab:CreateToggle({
     Name = "Nhảy Cao",
